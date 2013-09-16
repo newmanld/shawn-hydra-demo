8610d1824cdf0f7f14ef1084c50d8be0bfc45a45
Shawn M. Kiewel <smkiewel@uga.edu>
2013-09-16 16:12:20 -0400

    Initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraDemo::Application.config.secret_token = '8fff25d5ff305792326446890ea82e1c309937bb3ac3ba7c76ee9564e20645f02f2622962ad1223c9f0569613bd8e6d223c8abe3be42dde25a6b69b66bc3a2e2'
